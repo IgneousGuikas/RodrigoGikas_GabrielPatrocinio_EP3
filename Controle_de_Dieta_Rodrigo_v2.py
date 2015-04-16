6c05bf7438ca7e56f5967e15ab3120c579459b2f
IgneousGuikas <rodrigo.rgk@hotmail.com>
2015-04-16 13:02:56 -0300

    Update Controle_de_Dieta_Rodrigo_v2.py

--- Controle_de_Dieta_Rodrigo_v2.py
+++ Controle_de_Dieta_Rodrigo_v2.py
@@ -17,10 +17,9 @@ def processa_dados_alimentos(lista_alimentos):
     Recebe a lista de linhas do arquivo 'alimentos.csv' e devolve
     um dicionário com alimentos como keys e valores nutricionais
     como values na forma de listas
-    '''
-    
-    
-    ''' Testa se processa_dados_alimentos funciona adequadamente
+        
+        
+        Testa se processa_dados_alimentos funciona adequadamente
         >>> processa_dados_alimentos(lista_alimentos) == {'ABACATE': [100.0, 177.00, 1.80, 6.40, 16.00], 'ABACAXI': [100.0, 58.00, 0.40, 13.70, 0.20], 'ABACAXI EM CALDA': [100.0, 122.00, 0.44, 29.77, 0.18]}
         True
     '''
@@ -40,10 +39,9 @@ def processa_dados_usuario(lista_usuario):
     Recebe a lista de linhas do arquivo 'usuario.csv' e devolve
     um dicionário com as especificações do usuário e um dicionário
     com os alimentos ingeridos pelo usuário e suas quantidades
-    '''
-    
-    
-    ''' Testa se processa_dados_usuario funciona adequadamente
+        
+        
+        Testa se processa_dados_usuario funciona adequadamente
         >>> a = {}
         >>> b = {}
         >>> a,b = processa_dados_usuario(lista_usuario)
